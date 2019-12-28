53e01d5db94b5f6910fddfda360b5dd3668f8247
takuma10Mi <takumaii@yahoo.co.jp>
2019-12-28 19:24:14 +0900

    credit_card model

---
+++ db/migrate/20191228095546_create_credit_cards.rb
@@ -0,0 +1,12 @@
+class CreateCreditCards < ActiveRecord::Migration[5.2]
+  def change
+    create_table :credit_cards do |t|
+      t.integer :user_id, null:false,foreign_key: true
+      t.integer :card_number, null:false,unique: true
+      t.date :expiration_date_month, null:false
+      t.date :expiration_date_year, null:false
+      t.integer :security_code, null:false
+      t.timestamps
+    end
+  end
+end
--- spec/controllers/resister_controller_spec.rb
+++ spec/controllers/resister_controller_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe ResisterController, type: :controller do
+  
+  def new
+    @user = User.new
+  end
+  def create
+    @user = User.new(user_params)
+    flash[:success] = @user 
+      if @user.save
+        flash[:success] = '新しいユーザーを登録しました。'
+        redirect_to @user
+      else
+        flash.now[:danger] = 'ユーザーの登録に失敗しました。'
+        render :new
+      end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(user_params)
+
+  end
 
 end
