24bd71fa0b7cb6849e0e638ee17ed89031ff1355
takuma10Mi <takumaii@yahoo.co.jp>
2019-12-30 22:28:52 +0900

    Revert "comit"
    This reverts commit d0a4d2cb2ce06f2c848e1823b3d5be0527792c9e.

--- app/models/credit_card.rb
+++ app/models/credit_card.rb
@@ -1,10 +1,2 @@
 class CreditCard < ApplicationRecord
-
-
-  # validates :user_id, presence: true 
-  validates :card_number, presence: true 
-  validates :expiration_date_month, presence: true 
-  validates :expiration_date_year, presence: true 
-  validates :security_code, presence: true 
-
 end
--- db/migrate/20191228095546_create_credit_cards.rb
+++ db/migrate/20191228095546_create_credit_cards.rb
@@ -1,12 +1,12 @@
 class CreateCreditCards < ActiveRecord::Migration[5.2]
   def change
     create_table :credit_cards do |t|
-      t.references :user, foreign_key: true
-      t.string :card_number, null:false, null:false,unique: true
-      t.integer :expiration_date_month, null:false
-      t.integer :expiration_date_year, null:false
+      t.integer :user_id, null:false,foreign_key: true
+      t.integer :card_number, null:false,unique: true
+      t.date :expiration_date_month, null:false
+      t.date :expiration_date_year, null:false
       t.integer :security_code, null:false
-      t.timestamps null:false
+      t.timestamps
     end
   end
 end
