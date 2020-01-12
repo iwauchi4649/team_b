10b5b124784e62567c2976b1733dd2006065e741
hikaru08 <ur.mh08@gmail.com>
2020-01-12 16:46:37 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,13 +39,13 @@ namespace :deploy do
     invoke 'unicorn:start'
   end
 
-  desc 'upload secrets.yml'
+  desc 'upload credentials.yml.enc'
   task :upload do
     on roles(:app) do |host|
       if test "[ ! -d #{shared_path}/config ]"
         execute "mkdir -p #{shared_path}/config"
       end
-      upload!('config/secrets.yml', "#{shared_path}/config/secrets.yml")
+      upload!('config/credentials.yml.enc', "#{shared_path}/config/credentials.yml.enc")
     end
   end
   before :starting, 'deploy:upload'
