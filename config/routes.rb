4a680dd6ec9a62bd10bba8a277fd7cb45e879b97
takuma10Mi <takumaii@yahoo.co.jp>
2020-01-13 20:40:56 +0900

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,20 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    sessions: 'users/sessions'
+  devise_for :credit_cards, controllers: {
+    sessions: 'credit_cards/sessions',
+    passwords: 'credit_cards/passwords',
+    registration: 'credit_cards/registration'
   }
+  devise_for :addresses, controllers: {
+    sessions: 'addresses/sessions',
+    passwords: 'addresses/passwords',
+    registration: 'users/registration'
+  }
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    passwords: 'users/passwords',
+    registration: 'users/registration'
+  }
+
   get 'users/index'
   get 'users/show'
   get 'users/new'
---
+++ spec/factories/addres.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :addre do
+    
+  end
+end
