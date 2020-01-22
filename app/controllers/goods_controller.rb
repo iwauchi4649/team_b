ccc182cc02a13f1be9d07d355b42713ac71f2d8c
Syoma Iwauchi <syoma4645@gmail.com>
2020-01-22 15:13:25 +0900

    商品一覧表示_全てに対応
    テスト済み

--- app/controllers/goods_controller.rb
+++ app/controllers/goods_controller.rb
@@ -23,6 +23,7 @@ class GoodsController < ApplicationController
   end
 
   def index
+    # レディース新着アイテム
     # 写真とgood_idを紐付ける
     @lady_category_id = Category.find_by(name: "レディース").indirect_ids
     @lady_goods = Good.where(category_id: @lady_category_id).order('id DESC')
@@ -30,11 +31,45 @@ class GoodsController < ApplicationController
     # nameとfee用
     @lady_photo = Photo.where(good_id: @lady_goods_id).limit(10).order('id DESC')
 
+    # メンズ新着アイテム
+    @man_category_id = Category.find_by(name: "メンズ").indirect_ids
+    @man_goods = Good.where(category_id: @man_category_id).order('id DESC')
+    @man_goods_id = Good.where(category_id: @man_category_id).pluck(:id)
+    @man_photo = Photo.where(good_id: @man_goods_id).limit(10).order('id DESC')
+
+    # 家電・スマホ・カメラ新着アイテム
+    @electricity_category_id = Category.find_by(name: "家電・スマホ・カメラ").indirect_ids
+    @electricity_goods = Good.where(category_id: @electricity_category_id).order('id DESC')
+    @electricity_goods_id = Good.where(category_id: @electricity_category_id).pluck(:id)
+    @electricity_photo = Photo.where(good_id: @electricity_goods_id).limit(10).order('id DESC')
+
+    # おもちゃ・ホビー・グッズ新着アイテム
+    @hobby_category_id = Category.find_by(name: "おもちゃ・ホビー・グッズ").indirect_ids
+    @hobby_goods = Good.where(category_id: @hobby_category_id).order('id DESC')
+    @hobby_goods_id = Good.where(category_id: @hobby_category_id).pluck(:id)
+    @hobby_photo = Photo.where(good_id: @hobby_goods_id).limit(10).order('id DESC')
+
+    # シャネル新着アイテム
     # 写真とgood_idを紐付ける
     @good_chanel_id = Good.where(brand:"シャネル").pluck(:id)
     @photo_chanel_id = Photo.where(good_id: @good_chanel_id).limit(10).order('id DESC')
     # nameとfee用
     @chanel = Good.where(brand:"シャネル").order('id DESC')
+
+    # ルイヴィトン新着アイテム
+    @good_vuitton_id = Good.where(brand:"ルイヴィトン").pluck(:id)
+    @photo_vuitton_id = Photo.where(good_id: @good_vuitton_id).limit(10).order('id DESC')
+    @vuitton = Good.where(brand:"ルイヴィトン").order('id DESC')
+
+    # シュプリーム新着アイテム
+    @good_supreme_id = Good.where(brand:"シュプリーム").pluck(:id)
+    @photo_supreme_id = Photo.where(good_id: @good_supreme_id).limit(10).order('id DESC')
+    @supreme = Good.where(brand:"シュプリーム").order('id DESC')
+
+    # ナイキ新着アイテム
+    @good_nike_id = Good.where(brand:"ナイキ").pluck(:id)
+    @photo_nike_id = Photo.where(good_id: @good_nike_id).limit(10).order('id DESC')
+    @nike = Good.where(brand:"ナイキ").order('id DESC')
   end
 
   private
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,31 @@
 lady = Category.create(:name=>"レディース")
+mans = Category.create(:name=>"メンズ")
+appliances = Category.create(:name=>"家電・スマホ・カメラ")
+hobby = Category.create(name: "おもちゃ・ホビー・グッズ")
 
+# レディースの子カテゴリ
 lady_tops = lady.children.create(:name=>"トップス")
 lady_jacket = lady.children.create(:name=>"ジャケット/アウター")
-
+# レディースの孫カテゴリ
 lady_tops.children.create([{:name=>"Tシャツ/カットソー(半袖/袖なし)"}, {:name=>"Tシャツ/カットソー(七分/長袖)"},{:name=>"その他"}])
 lady_jacket.children.create([{:name=>"テーラードジャケット"}, {:name=>"ノーカラージャケット"}, {:name=>"Gジャン/デニムジャケット"},{:name=>"その他"}])
-
-mans = Category.create(:name=>"メンズ")
-
+# メンズの子カテゴリ
 mans_tops = mans.children.create(:name=>"トップス")
 mans_jacket = mans.children.create(:name=>"ジャケット/アウター")
-
+# メンズの孫カテゴリ
 mans_tops.children.create([{:name=>"Tシャツ/カットソー(半袖/袖なし)"}, {:name=>"Tシャツ/カットソー(七分/長袖)"},{:name=>"その他"}])
 mans_jacket.children.create([{:name=>"テーラードジャケット"}, {:name=>"ノーカラージャケット"}, {:name=>"Gジャン/デニムジャケット"},{:name=>"その他"}])
-
-appliances = Category.create(:name=>"家電・スマホ・カメラ")
-
+# 家電・スマホ・カメラの子カテゴリ
 appliances_phone = appliances.children.create(:name=>"スマートフォン/携帯電話")
 appliances_accessorie = appliances.children.create(:name=>"スマホ/アクセサリー")
-
+# 家電・スマホ・カメラの孫カテゴリ
 appliances_phone.children.create([{:name=>"スマートフォン本体"}, {:name=>"バッテリー/充電器"},{:name=>"その他"}])
 appliances_accessorie.children.create([{:name=>"Android用ケース"}, {:name=>"iPhone用ケース"}, {:name=>"カバー"},{:name=>"その他"}])
+# おもちゃ・ホビー・グッズの子カテゴリ
+hobby_toy = hobby.children.create(name: "おもちゃ")
+hobby_card = hobby.children.create(name: "トレーディングカード")
+hobby_figure = hobby.children.create(name: "フィギュア")
+# おもちゃ・ホビー・グッズの孫カテゴリ
+hobby_toy.children.create([{name: "キャラクターグッズ"}, {name: "ぬいぐるみ"}, {name: "小物/アクセサリー"}, {name: "模型/プラモデル"}, {name: "ミニカー"}, {name: "トイラジコン"}, {name: "プラモデル"}, {name: "ホビーラジコン"}, {name: "鉄道模型"}, {name: "その他"}])
+hobby_card.children.create([{name: "遊戯王"}, {name: "マジック：ザ・ギャザリング"}, {name: "ポケモンカードゲーム"}, {name: "デュエルマスターズ"}, {name: "バトルスピリッツ"}, {name: "プリパラ"}, {name: "アイカツ"}, {name: "カードファイト!!ヴァンガード"}, {name: "ヴァイスシュヴァルツ"}, {name: "プロ野球オーナーズリーグ"}, {name: "ベースボールヒーローズ"}, {name: "ドラゴンボール"}, {name: "スリーブ"}, {name: "その他"}])
+hobby_figure.children.create([{name: "コミック/アニメ"}, {name: "特撮"}, {name: "ゲームキャラクター"}, {name: "SF/ファンタジー/ホラー"}, {name: "アメコミ"}, {name: "スポーツ"}, {name: "ミリタリー"}, {name: "その他"}])
