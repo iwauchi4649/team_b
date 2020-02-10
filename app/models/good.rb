37b82abb0978d1a680f7f4d70889fadac49c4820
hikaru08 <ur.mh08@gmail.com>
2020-02-10 18:23:59 +0900

    Update good.rb

--- app/models/good.rb
+++ app/models/good.rb
@@ -7,6 +7,15 @@ class Good < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :evalutions, dependent: :destroy
   accepts_nested_attributes_for :photos, allow_destroy: true
+  validates :fee, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
+  validates :name, presence: true
+  validates :discription, presence: true
+  validates :condition, presence: true
+  validates :delivery_type, presence: true
+  validates :prefecture, presence: true
+  validates :day, presence: true
+  validates :fee, presence: true
+  validates_associated :photos
 
   def like_user(user_id)
     likes.find_by(user_id: user_id)
