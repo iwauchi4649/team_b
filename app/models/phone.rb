5b74d9bd8a10b42616f413f206e59ac7234b8a3a
takuma10Mi <takumaii@yahoo.co.jp>
2020-02-23 19:23:19 +0900

    commit

--- app/models/phone.rb
+++ app/models/phone.rb
@@ -1,5 +1,5 @@
 class Phone < ApplicationRecord
   
   belongs_to :user, optional: true
-  validates :number,presence: true
+  validates :number,presence: true,length: { in:8..12 } 
 end
--- spec/factories/phones.rb
+++ spec/factories/phones.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :phone do
-    
+    user_id {1}
+    number  {09012345678}
+    created_at {nil}
+    updated_at {nil}
+    good_id {1}
   end
 end
